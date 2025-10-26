2452f2b409ff1ca52963281c5b8959837a992998
Muhamad Muslih <muhamadmuslih720@gmail.com>
2025-10-26 13:56:05 +0700

    feat: add route POST /albums/{id}/covers for cover upload

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -24,6 +24,19 @@ const routes = (service) => {
       path: '/albums/{id}',
       handler: handler.deleteAlbumByIdHandler,
     },
+    {
+      method: 'POST',
+      path: '/albums/{id}/covers',
+      handler: handler.postAlbumCoverHandler,
+      options: {
+        payload: {
+          allow: 'multipart/form-data',
+          multipart: true,
+          output: 'stream',
+          maxBytes: 512000, // 500KB
+        },
+      },
+    },
   ];
 };
 
